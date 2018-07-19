38857e9b0cabeaa2ad4d1369f5fe788673ef7b54
fu-bear <iangen@aliyun.com>
2018-07-19 15:04:26 +0800

    代价函数

--- 代价函数.py
+++ 代价函数.py
@@ -1,6 +1,6 @@
-def computerCost(X,y,theta):
+def computerCost(X, y, theta):
     m = len(y)
     J = 0
 
-    J = (np.transpose(X*theta-y))*(X*theta-y)/(2*m) #计算代价函数
+    J = (np.transpose(X*theta-y))*(X*theta-y)/(2*m)  # 计算代价函数
     return J
